e4421fc128229e66ece92f68d9d4773db998c4fa
GreenBritain <newsweeties2020@protonmail.com>
2025-07-10 14:08:23 +0100

    remove: Remove footer component from all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -106,7 +105,6 @@ export default function RootLayout({
         <div className="flex-1">
           {children}
         </div>
-        <Footer />
       </body>
     </html>
   );
